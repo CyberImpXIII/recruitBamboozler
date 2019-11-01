e1459c0743b37a7ab1615dc35465ad550486e2d9
Jacob Nelsen-Epstein <jnelsenepstein@gmail.com>
2019-11-01 12:54:43 +0900

    added server file still need to get it running correctly

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const env = require('dotenv').config();
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/api/save', (req, res)=>{
+    fs.write('.env', 'test="test"')
+
+})
+
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log(`Server is listening on port ${process.env.PORT || 8080}`)
+});
