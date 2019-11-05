4cecfc487ca7905cc526bdd8a9d08f0f175d83b5
Jacob Nelsen-Epstein <jnelsenepstein@gmail.com>
2019-11-06 00:29:00 +0900

    added get request for test api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+  Axios.get('/api/save')
+  .then((res)=>(console.log(res)))
+  .catch((err)=>{console.log(err)});
   return (
     <div>
     </div>
