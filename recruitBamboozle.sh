d02fd9e8ef4c65e0c2c6f99c387d59c2fec745aa
Jacob Nelsen-Epstein <jnelsenepstein@gmail.com>
2020-02-18 13:22:54 -0600

    fixed absolute paths

--- recruitBamboozle.sh
+++ recruitBamboozle.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-date >> garbage
+date >> /Users/Specter/Desktop/Projects/codeProjects/personalProjects/recruit-bamboozler/garbage
 git add garbage
 git commit garbage -m 'this is automated'
+git push origin master
 echo 'done'
