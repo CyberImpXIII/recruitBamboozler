0ad361f22f168ccbe9523717e6749792e9894ac9
CyberImpXIII <jnelsenepstein@gmail.com>
2020-02-25 00:42:47 -0600

    starting to make this it should generate a more personalized script when this is complete

--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
-sudo crontab -l > chron.txt
-echo "*/1 * * * * /bin/bash /Users/Specter/Desktop/Projects/codeProjects/personalProjects/recruit-bamboozler/recruitBamboozle.sh" >> chron.txt
-sudo cron ./chron.txt
+echo "What is the directory that your repository is in?"
+read dir
+
+"#!/bin/bash
+declare -x SSH_AUTH_SOCK=$(find /var/folders/*/*/*/*/agent.* -type s | head -1)
+cd $dir
+date >> garbage
+git add garbage
+git commit garbage -m 'this is automated'
+git push origin master
+echo 'done'" > recruitBamboozletest.sh
+
+$currentDir = pwd
+
+crontab -l > chron.txt
+"* * * * * /bin/bash $currentDir/recruitBamboozle.sh" >> chron.txt
+cron chron.txt
