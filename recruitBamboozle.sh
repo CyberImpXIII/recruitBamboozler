a020109002e911558ae89403572ac63ee5f2d858
CyberImpXIII <jnelsenepstein@gmail.com>
2020-02-25 00:07:09 -0600

    maybe this will work now?

--- recruitBamboozle.sh
+++ recruitBamboozle.sh
@@ -2,7 +2,6 @@
 declare -x SSH_AUTH_SOCK=$(find /var/folders/*/*/*/*/agent.* -user Jacob -type s | head -1)
 cd /Users/Specter/Desktop/Projects/codeProjects/personalProjects/recruit-bamboozler
 date >> garbage
-# git remote set-url origin git@github.com:username/repo.git
 git add garbage
 git commit garbage -m 'this is automated'
 git push origin master
